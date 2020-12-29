8b3ccc834e7e81c95f614f6985308884ae979989
Ahmed the Green <eng.boshhab@gmail.com>
2020-12-29 21:07:52 +0300

    class work 8 part done successfully
    made array for thekr and array for counters and can add array for colors o spice upp the colors for the buttons :)

---
+++ CW-8-1/CW-8-1/CW_8_1App.swift
@@ -0,0 +1,17 @@
+//
+//  CW_8_1App.swift
+//  CW-8-1
+//
+//  Created by Ahmed Alkhuder on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CW_8_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
